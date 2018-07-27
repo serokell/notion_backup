aaef8b4064a3eb0a0d8d10e60524d882148ea3a6
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-27 11:58:35 +0300

    Parametrize path to DOCX template, commit template

--- lib/notion_to_gdrive/pandoc.ex
+++ lib/notion_to_gdrive/pandoc.ex
@@ -1,5 +1,8 @@
 defmodule NotionToGDrive.Pandoc do
-  def convert(input, template, output_directory) do
+  import ConfigMacro
+  config :notion_to_gdrive, template: "reference.docx"
+
+  def convert(input, output_directory) do
     output = Path.join(output_directory, Path.basename(input, ".md")) <> ".docx"
 
     System.cmd("pandoc", [
@@ -21,6 +24,6 @@ defmodule NotionToGDrive.Pandoc do
     output_directory = Briefly.create!(directory: true)
 
     for input_file <- input_files,
-        do: convert(input_file, "reference.docx", output_directory)
+        do: convert(input_file, output_directory)
   end
 end
