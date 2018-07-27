523c9b8f2bfa3142e97d328e23728a8d522e8375
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-27 11:55:51 +0300

    Add polling

--- lib/notion_to_gdrive.ex
+++ lib/notion_to_gdrive.ex
@@ -2,7 +2,7 @@ defmodule NotionToGDrive do
   use Application
 
   def start(_, _) do
-    children = []
+    children = [NotionToGDrive.Job]
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
--- lib/notion_to_gdrive/job.ex
+++ lib/notion_to_gdrive/job.ex
@@ -1,7 +1,26 @@
 alias NotionToGDrive.{Export, Import, Pandoc}
 
 defmodule NotionToGDrive.Job do
-  def run do
+  import ConfigMacro
+  config :notion_to_gdrive, interval: 1000 * 60 * 30
+
+  use GenServer
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def schedule(msec \\ 1) do
+    Process.send_after(self(), :tick, msec)
+  end
+
+  def init([]) do 
+    {:ok, schedule()}
+  end
+
+  def handle_info(:tick, old_timer) do
+    Process.cancel_timer(old_timer)
     Export.all_files() |> Pandoc.convert_all() |> Import.upload_files()
+    {:noreply, schedule(interval())}
   end
 end
